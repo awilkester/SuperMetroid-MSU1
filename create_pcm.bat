6f013ce29633a7d1def573c05c0dcefef95dce11
Michaël Larouche <michael.larouche@gmail.com>
2015-04-25 12:17:55 -0400

    PCM

--- create_pcm.bat
+++ create_pcm.bat
@@ -14,12 +14,12 @@ wav2msu -o supermetroid_msu1-9.pcm 09_statue_room.wav
 wav2msu -o supermetroid_msu1-10.pcm 10_samus_ship.wav
 wav2msu -o supermetroid_msu1-11.pcm -l 1139528 11_brinstar_vegetation.wav
 wav2msu -o supermetroid_msu1-12.pcm 12_brinstar_red_soil.wav
-wav2msu -o supermetroid_msu1-13.pcm 13_upper_norfair.wav
+wav2msu -o supermetroid_msu1-13.pcm -l 1932017 13_upper_norfair.wav
 wav2msu -o supermetroid_msu1-14.pcm 14_lower_norfair.wav
 wav2msu -o supermetroid_msu1-15.pcm 15_upper_maridia.wav
 wav2msu -o supermetroid_msu1-16.pcm 16_lower_maridia.wav
 wav2msu -o supermetroid_msu1-17.pcm 17_tourian.wav
-wav2msu -o supermetroid_msu1-18.pcm 18_mother_brain_battle.wav
+wav2msu -o supermetroid_msu1-18.pcm -l 1645110 18_mother_brain_battle.wav
 wav2msu -o supermetroid_msu1-19.pcm -l 1381486 19_big_boss_battle_1.wav
 wav2msu -o supermetroid_msu1-20.pcm -l 1343339 20_evacuation.wav
 wav2msu -o supermetroid_msu1-21.pcm 21_mysterious_statue_chamber.wav
@@ -31,5 +31,5 @@ wav2msu -o supermetroid_msu1-26.pcm 26_wrecked_ship_off.wav
 wav2msu -o supermetroid_msu1-27.pcm 27_wrecked_ship_on.wav
 wav2msu -o supermetroid_msu1-28.pcm -l 240291 28_theme_of_super_metroid.wav
 wav2msu -o supermetroid_msu1-29.pcm 29_death_cry.wav
-
+wav2msu -o supermetroid_msu1-30.pcm 30_ending.wav
 
